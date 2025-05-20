c5e2d8fcd0cee97a61f260d4ac16e177972450ce
andreamarciano <andrea.marciano.66@gmail.com>
2025-05-20 11:41:16 +0200

    background particles spawn & draw -> background.js

--- src/comp/game/SpaceInvaders/SpaceInvaders.jsx
+++ src/comp/game/SpaceInvaders/SpaceInvaders.jsx
@@ -10,8 +10,11 @@ import ControlsPopup from "./UI/ControlsPopup";
 import AudioSettings from "./UI/AudioSettings";
 import CanvasCenter from "./UI/CanvasCenter";
 
-/* Utils */
-import { createExplosion } from "./enemy/createExplosion";
+/* Particles */
+import {
+  spawnBackgroundParticles,
+  drawBackgroundParticles,
+} from "./particles/background";
 
 /* Player */
 import playerConfig from "./player/config";
@@ -25,6 +28,9 @@ import shipBubbleConfig from "./powerUp/ship/config";
 import { handleShipBubbleSpawn } from "./powerUp/ship/spawn";
 
 /* === Enemies === */
+// Utils
+import { createExplosion } from "./enemy/createExplosion";
+// Invader
 import invaderConfig from "./enemy/invader/config";
 import {
   spawnInvaderGrid,
@@ -440,14 +446,6 @@ function SpaceInvaders({ onClose }) {
   /* Particles */
   const particlesRef = useRef([]);
   const backgroundParticlesRef = useRef([]);
-  const backgroundParticles = {
-    radius: 2,
-    speed: 0.3,
-    speedBoss: 15,
-    speed2: 1.8,
-    opacity: 0.5,
-    color: "white",
-  };
   const isBoostingRef = useRef(false);
 
   /***************************************************************
@@ -727,27 +725,12 @@ function SpaceInvaders({ onClose }) {
     canvas.height = canvasSize.height;
 
     /* === BACKGROUND ANIMATION === */
-    const spawnBackgroundParticles = () => {
-      const particles = [];
-      for (let i = 0; i < 100; i++) {
-        particles.push({
-          x: Math.random() * canvas.width,
-          y: Math.random() * canvas.height,
-          radius: Math.random() * backgroundParticles.radius,
-          speedY: isBoostingRef.current
-            ? backgroundParticles.speedBoss
-            : bossDefeatedRef.current
-            ? backgroundParticles.speed2
-            : backgroundParticles.speed,
-          opacity:
-            backgroundParticles.opacity +
-            Math.random() * backgroundParticles.opacity,
-          color: backgroundParticles.color,
-        });
-      }
-      backgroundParticlesRef.current = particles;
-    };
-    spawnBackgroundParticles();
+    spawnBackgroundParticles({
+      canvas,
+      backgroundParticlesRef,
+      bossDefeatedRef,
+      isBoostingRef,
+    });
 
     /* === LOAD IMAGES === */
     const loadImages = () => {
@@ -1314,29 +1297,12 @@ function SpaceInvaders({ onClose }) {
       });
 
       /* === DRAW: BACKGROUND ANIMATION === */
-      backgroundParticlesRef.current.forEach((p) => {
-        // boosted
-        p.speedY = isBoostingRef.current
-          ? backgroundParticles.speedBoss
-          : bossDefeatedRef.current
-          ? backgroundParticles.speed2
-          : backgroundParticles.speed;
-
-        p.y += p.speedY;
-
-        if (p.y - p.radius > canvas.height) {
-          p.x = Math.random() * canvas.width;
-          p.y = -p.radius;
-        }
-
-        c.save();
-        c.globalAlpha = p.opacity;
-        c.beginPath();
-        c.arc(p.x, p.y, p.radius, 0, Math.PI * 2);
-        c.fillStyle = p.color;
-        c.fill();
-        c.closePath();
-        c.restore();
+      drawBackgroundParticles({
+        c,
+        canvas,
+        backgroundParticlesRef,
+        bossDefeatedRef,
+        isBoostingRef,
       });
 
       /***************************************************************
