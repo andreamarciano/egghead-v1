6a44cb31c6496f8f798e602b04d6c542653018b6
andreamarciano <andrea.marciano.66@gmail.com>
2025-05-25 17:39:22 +0200

    meh

--- src/pages/Cabinet/ArcadeCabinet.jsx
+++ src/pages/Cabinet/ArcadeCabinet.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./ArcadeCabinet.css";
 import Navbar from "../../comp/Navbar";
 import Cabinet from "./Cabinet";
@@ -7,24 +7,31 @@ import Cabinet from "./Cabinet";
 const cabinetImage = "/images/cabinet/cabinet.png";
 
 function ArcadeCabinet() {
-  const [zoomedIn, setZoomedIn] = useState(false);
+  const [zoomStage, setZoomStage] = useState("idle");
+
+  useEffect(() => {
+    if (zoomStage === "zooming") {
+      const timer = setTimeout(() => {
+        setZoomStage("entered");
+      }, 3000);
+      return () => clearTimeout(timer);
+    }
+  }, [zoomStage]);
 
   return (
     <>
       <Navbar />
       <div className="arcade-bg h-screen flex items-center justify-center relative overflow-hidden">
-        {!zoomedIn && (
-          <div
+        {zoomStage === "idle" && (
+          <motion.div
+            initial={{ scale: 1, y: 0 }}
+            animate={{ scale: 1, y: 0 }}
             className="relative w-72 md:w-96 cursor-pointer"
-            onClick={() => setZoomedIn(true)}
+            onClick={() => setZoomStage("zooming")}
           >
-            <motion.img
+            <img
               src={cabinetImage}
               alt="Arcade Cabinet"
-              initial={{ scale: 1, opacity: 1 }}
-              animate={{ scale: 1, opacity: 1 }}
-              exit={{ scale: 3, opacity: 0 }}
-              transition={{ duration: 1 }}
               className="arcade-cabinet"
             />
             <div className="press-start absolute top-[33%] left-[50.5%] -translate-x-1/2 text-center text-xl">
@@ -32,14 +39,34 @@ function ArcadeCabinet() {
               <br />
               <span>START</span>
             </div>
-          </div>
+          </motion.div>
         )}
 
-        {zoomedIn && (
+        {zoomStage === "zooming" && (
+          <motion.div
+            initial={{ scale: 1, y: 0 }}
+            animate={{ scale: 3, y: -10 }}
+            transition={{ duration: 2, ease: "easeInOut" }}
+            className="relative w-72 md:w-96"
+          >
+            <img
+              src={cabinetImage}
+              alt="Arcade Cabinet"
+              className="arcade-cabinet"
+            />
+            <div className="press-start absolute top-[33%] left-[50.5%] -translate-x-1/2 text-center text-xl">
+              <span>PRESS</span>
+              <br />
+              <span>START</span>
+            </div>
+          </motion.div>
+        )}
+
+        {zoomStage === "entered" && (
           <motion.div
             initial={{ opacity: 0, scale: 0.8 }}
             animate={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 0.8 }}
+            transition={{ duration: 1 }}
             className="absolute inset-0"
           >
             <Cabinet />
@@ -49,4 +76,5 @@ function ArcadeCabinet() {
     </>
   );
 }
+
 export default ArcadeCabinet;
